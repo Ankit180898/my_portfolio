5e3f2ab8bedf4880538a92217b89f46c02978ca0
Ankit180898 <karan180898@gmail.com>
2024-02-12 19:36:19 +0530

    Some changes made

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,90 +15,89 @@ class HomeScreen extends StatelessWidget {
           preferredSize: Size(size.width, size.height * 0.20),
           child: TopBarContents()),
       // drawer: ,
-      body: Column(
-        children: [
-          Center(
-              child: Stack(
-            children: [
-              Image.asset(
-                "assets/heading_image.png",
-                height: size.height * 0.5,
-                width: size.width * 0.6,
-              ),
-              Positioned(
-                  top: 100,
-                  left: 200,
-                  child: Image.asset(
-                    "assets/linkedin-logo.png",
-                    height: size.height * 0.050,
-                    width: size.width * 0.050,
-                  )),
-              Positioned(
-                  top: 200,
-                  left: 200,
-                  child: Image.asset(
-                    "assets/github.png",
-                    height: size.height * 0.050,
-                    width: size.width * 0.050,
-                  )),
-              Positioned(
-                  top: 300,
-                  left: 200,
-                  child: Image.asset(
-                    "assets/dribbble-logo.png",
-                    height: size.height * 0.050,
-                    width: size.width * 0.050,
-                  )),
-            ],
-          )),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const SizedBox(width: 20.0, height: 100.0),
-              const Text(
-                'Hi, I\'m Ankit, A',
-                style: TextStyle(fontSize: 43.0, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(width: 20.0, height: 100.0),
-              DefaultTextStyle(
-                style: const TextStyle(
-                  fontSize: 43.0,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+                child: Stack(
+              children: [
+                Image.asset(
+                  "assets/heading_image.png",
+                  height: size.height * 0.5,
+                  width: size.width * 0.6,
                 ),
-                child: AnimatedTextKit(
-                  isRepeatingAnimation: true,
-                  repeatForever: true,
-                  animatedTexts: [
-                    WavyAnimatedText('Developer',
-                        textStyle: TextStyle(
-                            fontSize: 43.0, fontWeight: FontWeight.bold)),
-                    WavyAnimatedText('/Designer',
-                        textStyle: TextStyle(
-                            fontSize: 43.0, fontWeight: FontWeight.bold)),
-                  ],
-                  onTap: () {
-                    print("Tap Event");
-                  },
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          SizedBox(
-            height: 100,
-            width: size.width * 0.35,
-            child: Text(
-              "Passionate Flutter developer dedicated to crafting seamless cross-platform solutions.",
-            maxLines: 5,
-              textAlign: TextAlign.center,
+                Positioned(
+                    top: 100,
+                    left: 200,
+                    child: Image.asset(
+                      "assets/linkedin-logo.png",
+                      height: size.height * 0.050,
+                      width: size.width * 0.050,
+                    )),
+                Positioned(
+                    top: 200,
+                    left: 200,
+                    child: Image.asset(
+                      "assets/github.png",
+                      height: size.height * 0.050,
+                      width: size.width * 0.050,
+                    )),
+                Positioned(
+                    top: 300,
+                    left: 200,
+                    child: Image.asset(
+                      "assets/dribbble-logo.png",
+                      height: size.height * 0.050,
+                      width: size.width * 0.050,
+                    )),
+              ],
+            )),
+            const SizedBox(
+              height: 10,
             ),
-          )
-        ],
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(width: 20.0, height: 100.0),
+                const Text(
+                  'Hi, I\'m Ankit, A',
+                  style: TextStyle(fontSize: 43.0, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(width: 20.0, height: 100.0),
+                DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 43.0,
+                  ),
+                  child: AnimatedTextKit(
+                    isRepeatingAnimation: true,
+                    repeatForever: true,
+                    animatedTexts: [
+                      WavyAnimatedText('Developer',
+                          textStyle: TextStyle(
+                              fontSize: 43.0, fontWeight: FontWeight.bold)),
+                   ],
+                    onTap: () {
+                      print("Tap Event");
+                    },
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 100,
+              width: size.width * 0.35,
+              child: Text(
+                "Passionate Flutter developer dedicated to crafting seamless cross-platform solutions.",
+              maxLines: 5,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
